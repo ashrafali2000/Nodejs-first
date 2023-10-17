d72d5eb051ae888c060d1b2a54441eb7f8861e2e
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-17 14:08:39 +0500

    just practice

--- myCheckFolder/check.js
+++ myCheckFolder/check.js
@@ -1,8 +1,7 @@
-module.exports.data1 = "I from checkFile data1"
-
-const data0 = "I from checkFile data0"
-const data2 = "I from checkFile data2"
-const data3 = "I from checkFile data3"
-
-module.exports= {data0 , data2 , data3};
+const func = () => {
+    console.log("I from Func");
+}
+const data = "I from checkFile data1"
+module.exports = data ;
+module.exports = func
 
