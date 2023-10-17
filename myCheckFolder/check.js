5910dc638c0d5b753ebed93428b22a5aa479ed05
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-17 14:00:27 +0500

    creatre myCheckFolder

---
+++ myCheckFolder/check.js
@@ -0,0 +1,8 @@
+module.exports.data1 = "I from checkFile data1"
+
+const data0 = "I from checkFile data0"
+const data2 = "I from checkFile data2"
+const data3 = "I from checkFile data3"
+
+module.exports= {data0 , data2 , data3};
+
