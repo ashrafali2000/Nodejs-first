eef88f2e858a9e862c6eecbf0a75bc7066142776
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-20 22:18:09 +0500

    commit all the old code

--- app.js
+++ app.js
@@ -4,35 +4,35 @@
 // const justCheck = require("./myCheckFolder/check")
 // console.log(justCheck)
 //module
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
-const filePath = path.join(process.cwd(), "formData.txt");
+// const http = require("http");
+// const fs = require("fs");
+// const path = require("path");
+// const filePath = path.join(process.cwd(), "formData.txt");
 
 // Create Server
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("Hello User");
-    res.end();
-  } else if (req.url === "/form") {
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.write("Hello User");
+//     res.end();
+//   } else if (req.url === "/form") {
     // res.setHeader("Content-Type", "text/html");
-    res.write(`<h1>WellCome to Our Website</h1>
-    <div class ='myForm'>
-    <form action ='/submit' method ='POST'>
-    <lable for='firstName'>First_Name</lable>
-    <input type='text' name='firstName' id='firstName' required/><br><br>
-    <lable for='lastName'>Last_Name</lable>
-    <input type='text' name='lastName' id='lastName' required/><br><br>
-    <lable for='email'>Email</lable>
-    <input type='email' name='email' id = 'email' required/><br><br>
-    <lable for= 'password'>Password</lable>
-    <input type='password' name='password' id= 'password' required/><br><br>
-    <button> Submit </button>
-    </form>
-    </div>
-    `);
-    res.end();
-  } else if (req.url === "/submit") {
+  //   res.write(`<h1>WellCome to Our Website</h1>
+  //   <div class ='myForm'>
+  //   <form action ='/submit' method ='POST'>
+  //   <lable for='firstName'>First_Name</lable>
+  //   <input type='text' name='firstName' id='firstName' required/><br><br>
+  //   <lable for='lastName'>Last_Name</lable>
+  //   <input type='text' name='lastName' id='lastName' required/><br><br>
+  //   <lable for='email'>Email</lable>
+  //   <input type='email' name='email' id = 'email' required/><br><br>
+  //   <lable for= 'password'>Password</lable>
+  //   <input type='password' name='password' id= 'password' required/><br><br>
+  //   <button> Submit </button>
+  //   </form>
+  //   </div>
+  //   `);
+  //   res.end();
+  // } else if (req.url === "/submit") {
 
     // res.write(Data)
     // let Data = "";
@@ -57,33 +57,37 @@ const server = http.createServer((req, res) => {
     // })
     
     //ATSP  - (Apny Tarap Say Practice)
-    let Storage = "";
-    req.on("data", chunk => Storage += chunk);
-    req.on("end", () => {
+    // let Storage = "";
+    // req.on("data", chunk => Storage += chunk);
+    // req.on("end", () => {
         // console.log(Storage)
         // res.end()
-      fs.readFile(filePath, "utf8", (err, formFileData) => {
-        {
-          if (err) {
-            res.write(err);
-            res.end();
-          }
+      // fs.readFile(filePath, "utf8", (err, formFileData) => {
+      //   {
+      //     if (err) {
+      //       res.write(err);
+      //       res.end();
+      //     }
         //   console.log(Storage);
-          let newData = formFileData + "\n" + Storage;
-          fs.writeFile(filePath, newData, "utf8", (err) => {
-            if (err) {
-              res.write(err);
-              res.end();
-            }
-            res.write("Data Recieved Sucessfully");
-            res.end();
-          });
-        }
-      });
-    });
-  } else {
-    res.write("404 Error Page not Found");
-    res.end();
-  }
-});
-server.listen(3000);
+//           let newData = formFileData + "\n" + Storage;
+//           fs.writeFile(filePath, newData, "utf8", (err) => {
+//             if (err) {
+//               res.write(err);
+//               res.end();
+//             }
+//             res.write("Data Recieved Sucessfully");
+//             res.end();
+//           });
+//         }
+//       });
+//     });
+//   } else {
+//     res.write("404 Error Page not Found");
+//     res.end();
+//   }
+// });
+// server.listen(3000);
+
+
+// NODEjs With Express
+
