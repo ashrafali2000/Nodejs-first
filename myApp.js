dd992515e58b83a5b9ee3c6221ff28c69cfa47e5
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-20 22:52:40 +0500

    add users with url

--- myApp.js
+++ myApp.js
@@ -2,9 +2,23 @@ import express from "express"
 const app = express();
 const PORT = 3000;
 
+const users = [
+    {
+        name: "ashraf",
+        email:"ashraf42@gmail.com"
+    },
+    {
+        name: "ali",
+        email:"ali42@gmail.com"
+    },
+    {
+        name: "khan",
+        email:"khan42@gmail.com"
+    }
+]
 //Get Request
-app.get("/ashraf", (req, res) => {
-        res.send("Hell World");
+app.get("/users", (req, res) => {
+        res.send(users);
 })
 
 // Server listen
