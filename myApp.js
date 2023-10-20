d692f222d76a76400e1665f6fb020a9fac5ac710
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-21 00:21:37 +0500

    add delete request code

--- myApp.js
+++ myApp.js
@@ -4,14 +4,17 @@ const PORT = 3000;
 
 const users = [
   {
+    id:1,
     name: "ashraf",
     email: "ashraf42@gmail.com",
   },
   {
+    id:2,
     name: "ali",
     email: "ali42@gmail.com",
   },
   {
+    id:3,
     name: "khan",
     email: "khan42@gmail.com",
   },
@@ -29,9 +32,17 @@ app.get("/users", (req, res) => {
 app.post("/users", (req, res) => {
   // console.log("req----> ", req);
   //   console.log("req----> ", req.body);
-  users.push(req.body)
+  users.push({id: users.length+1, ...req.body})
   res.send({message:"user added sucessfully"})
 });
+
+// Delete Request
+app.delete("/users/:id", (req, res)=> {
+// console.log("req---->", req.params)
+let index = users.findIndex(usr => usr.id === +req.params.id);
+users.splice(index, 1)
+res.send({message:"user deleted sucessfully"})
+})
 
 // Server listen
 app.listen(PORT, () => {
