9583beba4f33e739e9d14259b37e997eedfb7e82
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-20 22:36:43 +0500

    create appmjs file

---
+++ app.mjs
@@ -0,0 +1,11 @@
+// NODEjs With Express
+
+// Create express Server 
+// use module (import and export)
+// import express from "express"
+// const app = express();
+// const PORT = 3000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on Port ${PORT}`);
+// })
